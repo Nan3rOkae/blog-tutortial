778810c1cbcb06c7f5cfae51272a65ca928e49e8
Nana Kofi Okae <okaen@vcu.edu>
2024-04-04 23:40:35 -0400

    potentially fixed issue with pages not loading

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,10 +3,11 @@ import { ModeToggle } from "./ModeToggle";
 
 export default function Navbar() {
   return (
-    <nav className="w-full relative flex items-center justify-between max-w-4xl mx-auto px-4 py-5">
+    <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
       <Link href="/" className="font-bold text-3xl">
         Nana's<span className="text-primary">Blog</span>
       </Link>
+
       <ModeToggle />
     </nav>
   );
